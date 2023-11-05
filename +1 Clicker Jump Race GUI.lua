c6b9686ac369ca6c819e2a8bb854634173e77037
Elsie-Kgafela <133494419+Elsie-Kgafela@users.noreply.github.com>
2023-11-05 18:11:02 +0200

    update file

---
+++ +1 Clicker Jump Race GUI.lua
@@ -0,0 +1,406 @@
+--[[
+Game : https://www.roblox.com/games/12074193845
+Codded by : Keathunsar : https://github.com/dady172172/Roblox-Cheats : https://discord.gg/MhMB3c2CBn
+GUI Made by : xTheAlex14 : https://teppyboy.github.io/Mirrors/Documentations/Zypher_UI/zypher.wtf/docs/uilibdocs.html
+]]--
+---- vars ----
+kVars = {}
+kVars.WindowName = "+1 Clicker Jump Race GUI"
+kVars.lp = game:GetService('Players').LocalPlayer
+kVars.vu = game:GetService('VirtualUser')
+kVars.uis = game:GetService('UserInputService')
+kVars.rs = game:GetService('ReplicatedStorage')
+kVars.humanoid = kVars.lp.Character:WaitForChild('Humanoid')
+kVars.hrp = kVars.lp.Character:WaitForChild('HumanoidRootPart')
+
+---- destroy old gui if exists ----
+if game:GetService("CoreGui"):FindFirstChild(kVars.WindowName) then
+    game:GetService("CoreGui"):FindFirstChild(kVars.WindowName):Destroy()
+    wait(2)
+end
+
+---- antiAFK ----
+kVars.connectAntiAfk = game:GetService('Players').LocalPlayer.Idled:connect(function()
+    kVars.vu:CaptureController()
+    kVars.vu:ClickButton2(Vector2.new())
+ end)
+
+---- gui build ----
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/teppyboy/RbxScripts/master/Misc/UI_Libraries/Zypher/Library.lua"))()
+local Window = library:CreateMain({
+    projName = kVars.WindowName,
+    Resizable = true,
+    MinSize = UDim2.new(0,400,0,400),
+    MaxSize = UDim2.new(0,750,0,500),
+})
+
+local pageMain = Window:CreateCategory("Main")
+local sectionFarm = pageMain:CreateSection("Farm")
+
+local pageEggs = Window:CreateCategory("Eggs/Pets")
+local sectionEggs = pageEggs:CreateSection("Eggs")
+
+local pageTeleport = Window:CreateCategory("Teleport")
+local sectionTPToPlayer = pageTeleport:CreateSection("Teleport To Player")
+
+local pageCharacter = Window:CreateCategory("Character")
+local sectionCharacter = pageCharacter:CreateSection("Options")
+
+local pageMisc = Window:CreateCategory("Misc")
+local sectionKeybinds = pageMisc:CreateSection("KeyBinds")
+local sectionWorld = pageMisc:CreateSection("World Options")
+local sectionMisc = pageMisc:CreateSection("Options")
+
+local pageCredits = Window:CreateCategory("Credits")
+local sectionCreditsKeath = pageCredits:CreateSection("Coded by : Keathunsar")
+local sectionCreditsAlex = pageCredits:CreateSection("UI-Lib by : xTheAlex14")
+
+----========== page main ==========----
+---- Farm ----
+sectionFarm:Create("Toggle", "Click",function(bool)
+    kVars.boolClick = bool
+    if bool then
+        fClick()
+    end
+end,{default = kVars.boolClick})
+
+function fClick()
+    spawn(function()
+        while kVars.boolClick do
+            wait()
+            local args = {
+                [1] = game:GetService("Players").LocalPlayer,
+                [2] = 0
+            }
+
+            game:GetService("Players").LocalPlayer.PlayerGui.StarterJump.click.startbonus.LocalScript.event.click:FireServer(unpack(args))
+
+        end
+    end)
+end
+
+sectionFarm:Create("Toggle", "Win",function(bool)
+    kVars.boolWin = bool
+    if bool then
+        fWin()
+    end
+end,{default = kVars.boolWin})
+
+kVars.Wins = tonumber(kVars.lp.leaderstats.Wins.Value)
+function fWin()
+    spawn(function()
+
+        while kVars.boolWin do
+            
+            kVars.hrp.CFrame = game:GetService("Workspace").WinParts.super1.CFrame
+            wait()
+            
+        end
+    end)
+end
+
+sectionFarm:Create("Toggle", "Rebirth",function(bool)
+    kVars.boolRebirth = bool
+    if bool then
+        fRebirth()
+    end
+end,{default = kVars.boolRebirth})
+
+local function toNum(n)
+	if type(n) == "number" then return n end
+    n = string.gsub(n, ",", "")
+	return tonumber(n)
+end
+
+function fRebirth()
+    spawn(function()
+        while kVars.boolRebirth do
+            wait()			
+			if toNum(kVars.lp.leaderstats.Wins.Value) >= toNum(kVars.lp.PlayerGui.sidebuttons.RebirthFrame.textframe["2amountreq"].Text) then
+				kVars.lp.PlayerGui.sidebuttons.RebirthFrame.Frame:FindFirstChild("1Earth").RebirthButton.LocalScript.Script.Rebirth:FireServer(kVars.lp)
+			end
+        end
+    end)
+end
+
+
+sectionFarm:Create("Toggle", "Super Rebirth",function(bool)
+    kVars.boolSuperRebirth = bool
+    if bool then
+        fSuperRebirth()
+    end
+end,{default = kVars.boolSuperRebirth})
+
+function fSuperRebirth()
+    spawn(function()
+        while kVars.boolSuperRebirth do
+            wait()
+			if toNum(kVars.lp.leaderstats.Rebirths.Value) >= 50 then
+				kVars.lp.PlayerGui.sidebuttons.SuperRebirthFrame.Frame:FindFirstChild("1Earth").RebirthButton.LocalScript.Script.SR:FireServer(kVars.lp,50,kVars.lp.leaderstats.Rebirths)
+			end
+        end
+    end)
+end
+
+
+----========== page Eggs/Pets ==========----
+---- section eggs ----
+kVars.eggsList = {}
+kVars.eggsDisplay = {}
+for i,v in pairs(game:GetService("Workspace").gameEggs:GetChildren()) do
+    local tmpA = tonumber(v.GroundPlate.SurfaceGui.EggPriceText.eggPrice.Value)
+    table.insert(kVars.eggsList, {name = v.Name, displayName = v.Name .. " - " .. tmpA, path = v, cost = tmpA})
+    table.insert(kVars.eggsDisplay, v.Name .. " - " .. tmpA)
+end
+
+table.sort(kVars.eggsList, function(a,b)
+    return a.cost< b.cost
+end)
+
+for i,v in pairs(kVars.eggsList) do
+    table.insert(kVars.eggsDisplay, v.displayName)
+end
+
+kVars.selectedEgg = kVars.eggsDisplay[1]
+
+sectionEggs:Create("DropDown", "Select an egg to open", function(value)
+    kVars.selectedEgg = value
+end,{options = kVars.eggsDisplay, default = kVars.eggsDisplay[1], search = true})
+
+sectionEggs:Create("Toggle", "Open",function(bool)
+    kVars.boolOpenEgg = bool
+    if bool then
+        fOpenEgg()
+    end
+end,{default = kVars.boolOpenEgg})
+
+function fOpenEgg()
+    spawn(function()
+        while kVars.boolOpenEgg do
+            wait()
+            for i,v in pairs(kVars.eggsList) do
+                if v.displayName == kVars.selectedEgg then
+                    if v.cost <= tonumber(kVars.lp.leaderstats.Wins.Value) and  tonumber(kVars.lp.ActionValues.CurrentInventorySpace.Value) < tonumber(kVars.lp.ActionValues.TotalInventorySpace.Value) then
+                        kVars.rs.ServerEvents.hatchEgg:FireServer(v.cost, v.path, "Single")
+                    end
+                end
+                
+            end
+            
+        end
+    end)
+end
+
+sectionEggs:Create("Toggle", "",function(bool)
+    kVars.bool = bool
+    if bool then
+        f()
+    end
+end,{default = kVars.bool})
+
+function f()
+    spawn(function()
+        while kVars.bool do
+            wait()
+            
+        end
+    end)
+end
+
+
+----========== page teleport ==========----
+---- section teleport to player ----
+kVars.SelectedTPToPlayer = kVars.lp.name
+sectionTPToPlayer:Create("TextBox", "Enter Users Name", function(value)
+    kVars.SelectedTPToPlayer = value
+end,{text = "Enter Users Name"})
+
+sectionTPToPlayer:Create("Button", "Teleport To Player", function()
+    for i,v in pairs(game.Players:GetPlayers()) do
+        if kVars.SelectedTPToPlayer ~= nil and kVars.SelectedTPToPlayer ~= kVars.lp.name then
+            if string.lower(v.DisplayName) == string.lower(kVars.SelectedTPToPlayer) then
+                kVars.hrp.CFrame = v.Character.HumanoidRootPart.CFrame                
+            end
+        end
+    end
+end,{animated = true})
+
+
+----========== page character ==========----
+---- section Character ----
+kVars.walkSpeed = kVars.humanoid.WalkSpeed
+sectionCharacter:Create("Slider", "Walk Speed", function(value)
+    kVars.walkSpeed = value
+    kVars.humanoid.WalkSpeed = value
+end,{min = 16, max = 500, default = kVars.humanoid.walkSpeed, precise = false, changablevalue = true})
+
+kVars.jumpHeight = kVars.humanoid.JumpHeight
+sectionCharacter:Create("Slider", "Jump Height", function(value)
+    kVars.jumpHeight = value
+    kVars.humanoid.JumpHeight = value
+end,{min = 7.2, max = 500, default = kVars.humanoid.jumpHeight, precise = true, changablevalue = true})
+
+
+kVars.plrAdded = game.Players.LocalPlayer.CharacterAdded:Connect(function(child)
+    kVars.humanoid = kVars.lp.Character:WaitForChild('Humanoid', 999999)
+    kVars.hrp = kVars.lp.Character:WaitForChild('HumanoidRootPart', 999999)
+    task.wait(1)
+    kVars.humanoid.WalkSpeed = kVars.walkSpeed
+    kVars.humanoid.JumpHeight = kVars.jumpHeight
+end)
+
+kVars.boolInfJump = false
+sectionCharacter:Create("Toggle", "Inf Jump",function(bool)
+    kVars.boolInfJump = bool
+end,{default = kVars.boolInfJump})
+
+kVars.connectJumpRequest = game:GetService("UserInputService").JumpRequest:Connect(function()
+	if kVars.boolInfJump then
+		kVars.humanoid:ChangeState("Jumping")
+	end
+end)
+
+----========== page misc ==========----
+---- section keybinds ----
+sectionMisc:Create("Button", "Destroy this GUI",function()
+    game:GetService("CoreGui"):FindFirstChild(kVars.WindowName):Destroy()
+end,{animated = true})
+
+kVars.OpenCloseMenuKey = Enum.KeyCode.F5
+sectionKeybinds:Create("KeyBind", "Open Close Menu", function(key)
+    kVars.OpenCloseMenuKey = key
+end,{default = kVars.OpenCloseMenuKey})
+
+
+kVars.connectInputBegan = kVars.uis.InputBegan:Connect(function(key)
+    if key.UserInputType == Enum.UserInputType.Keyboard and key.KeyCode == kVars.OpenCloseMenuKey then
+        if game:GetService("CoreGui"):FindFirstChild(kVars.WindowName).Enabled then
+            game:GetService("CoreGui"):FindFirstChild(kVars.WindowName).Enabled = false
+        else
+            game:GetService("CoreGui"):FindFirstChild(kVars.WindowName).Enabled = true
+        end
+    end
+end)
+
+---- section world ----
+if game.Lighting.FogStart < 100 then
+    kVars.boolFog = true
+else
+    kVars.boolFog = false
+end
+sectionWorld:Create("Toggle", "Fog",function(bool)
+    kVars.boolFog = bool
+    if bool then
+        game.Lighting.FogStart = 0
+    else
+        game.Lighting.FogStart = math.huge
+    end
+end,{default = kVars.boolFog})
+
+---- sections options ----
+sectionMisc:Create("Button", "Destroy this GUI",function()
+    game:GetService("CoreGui"):FindFirstChild(kVars.WindowName):Destroy()
+end,{animated = true})
+
+sectionMisc:Create("Toggle", "Purchase Prompt",function(bool)
+    game:GetService("CoreGui").PurchasePrompt.Enabled = bool
+end,{default = game:GetService("CoreGui").PurchasePrompt.Enabled})
+
+kVars.Esp = {}
+kVars.boolEsp = false
+sectionMisc:Create("Toggle", "Player ESP",function(bool)
+    kVars.boolEsp = bool
+    if not bool then
+        for i,v in pairs(game.Players:GetPlayers()) do
+            if kVars.Esp[v] then
+                kVars.Esp[v].Drawing:Remove()
+            end
+        end
+        kVars.Esp = {}
+    else
+        fEsp()
+    end
+end,{default = kVars.boolEsp})
+
+kVars.plrRemovingConnect = game:GetService("Players").PlayerRemoving:Connect(function(player)
+    if kVars.Esp[player] then
+        kVars.Esp[player].Drawing:Remove()
+    end
+end)
+
+function fEsp()
+    spawn(function()
+        while kVars.boolEsp do
+            task.wait()
+            pcall(function()
+                for i,v in pairs(game.Players:GetPlayers()) do
+                    if kVars.boolEsp == false then break end
+                    if kVars.lp.name ~= v.name and kVars.boolEsp then
+                        if not kVars.Esp[v] then
+                            kVars.Esp[v] = {}
+                            kVars.Esp[v].Drawing = Drawing.new("Text")
+                            kVars.Esp[v].Drawing.Visible = false
+                            kVars.Esp[v].Drawing.Size = 16
+                            kVars.Esp[v].Drawing.Color = Color3.fromRGB(0, 255, 60)
+                            kVars.Esp[v].Drawing.Transparency = 1
+                            kVars.Esp[v].Drawing.ZIndex = 1
+                            kVars.Esp[v].Drawing.Center = true
+                            kVars.Esp[v].Drawing.Font = 3
+                            kVars.Esp[v].Drawing.Outline = true
+                            kVars.Esp[v].Drawing.OutlineColor = Color3.fromRGB(0,0,0)
+                            kVars.Esp[v].Drawing.Text = v.name
+                        end
+                        if v.Character:FindFirstChild("Head") then
+                            local vector, onScreen = game.Workspace.CurrentCamera:WorldToScreenPoint(game.Players[v.name].Character.Head.Position)
+                            if onScreen then
+                                kVars.Esp[v].Drawing.Visible = true
+                                kVars.Esp[v].Drawing.Position = Vector2.new(vector.x, vector.y)
+                            else
+                                kVars.Esp[v].Drawing.Visible = false
+                            end
+                        end
+                    end
+                end
+            end)
+        end
+    end)
+end
+
+
+----========== page credits ==========----
+---- keaths ----
+sectionCreditsKeath:Create("Button", "https://github.com/dady172172/Roblox-Cheats", function()
+    setclipboard('https://github.com/dady172172/Roblox-Cheats')
+end,{animated = true})
+
+sectionCreditsKeath:Create("Button", "https://discord.gg/MhMB3c2CBn", function()
+    setclipboard('https://discord.gg/MhMB3c2CBn')
+end,{animated = true})
+
+---- alex ----
+sectionCreditsAlex:Create("Button", "https://teppyboy.github.io/", function()
+    setclipboard('https://teppyboy.github.io/Mirrors/Documentations/Zypher_UI/zypher.wtf/docs/main.html')
+end,{animated = true})
+
+----========== set window size after load ==========----
+game:GetService("CoreGui"):FindFirstChild(kVars.WindowName).Motherframe.Size = UDim2.new(0, 495, 0, 400)
+
+----========== delete script if re-injecting ==========----
+
+kVars.cR = game:GetService("CoreGui").ChildRemoved:Connect(function(child)
+    if child.name == kVars.WindowName then
+        for i,v in pairs(kVars) do
+            if type(v) == "boolean" then
+                kVars[i] = false
+            end
+        end
+        kVars.connectAntiAfk:Disconnect()
+        kVars.connectInputBegan:Disconnect()
+        kVars.connectJumpRequest:Disconnect()
+        kVars.plrAdded:Disconnect()
+        kVars.plrRemovingConnect:Disconnect()
+        wait(1)
+        script:Destroy()
+        kVars.cR:Disconnect()
+    end
+end)
